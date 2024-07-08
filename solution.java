2d5b38d08d478b5582d10fa870647485f6fc270d
Kooreis <kooreis.official@gmail.com>
2024-07-08 12:08:06 -0500

    This is the BinaryTree class definition. It has a root node and two methods: isSymmetric and isMirror. The isSymmetric method checks if the binary tree is symmetric by calling the isMirror method. The isMirror method checks if two nodes are mirror images of each other.

--- solution.java
+++ solution.java
@@ -1,11 +1,17 @@
-import java.util.*;
+class BinaryTree {
+    Node root;
 
-class Node {
-    int value;
-    Node left, right;
+    boolean isSymmetric(Node node) {
+        return isMirror(node, node);
+    }
 
-    Node(int item) {
-        value = item;
-        left = right = null;
+    boolean isMirror(Node node1, Node node2) {
+        if (node1 == null && node2 == null)
+            return true;
+
+        if (node1 != null && node2 != null && node1.value == node2.value)
+            return (isMirror(node1.left, node2.right) && isMirror(node1.right, node2.left));
+
+        return false;
     }
 }
