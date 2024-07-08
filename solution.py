2f8d74faa86f05c3788593d406f561ac6b4d7c41
Kooreis <kooreis.official@gmail.com>
2024-07-08 12:08:06 -0500

    This update adds the 'is_mirror' function. This function checks if two given trees are mirror images of each other. It does this by recursively comparing the nodes of the two trees.

--- solution.py
+++ solution.py
@@ -1,5 +1,6 @@
-class Node:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
+def is_mirror(node1, node2):
+    if node1 is None and node2 is None:
+        return True
+    if node1 is None or node2 is None:
+        return False
+    return (node1.val == node2.val) and is_mirror(node1.right, node2.left) and is_mirror(node1.left, node2.right)
